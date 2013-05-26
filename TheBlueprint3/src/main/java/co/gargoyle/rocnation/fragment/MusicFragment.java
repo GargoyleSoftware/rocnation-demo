d3aadc8bf08d21972e9d8378611226d4a50e4721
David Young-Chan Kay <davidykay@gmail.com>
2013-05-26 15:54:46 -0400

    added all tracks to the db.

--- TheBlueprint3/src/main/java/co/gargoyle/rocnation/fragment/MusicFragment.java
+++ TheBlueprint3/src/main/java/co/gargoyle/rocnation/fragment/MusicFragment.java
@@ -1,30 +1,13 @@
 package co.gargoyle.rocnation.fragment;
 
-import java.util.Locale;
-
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
-import android.content.res.Configuration;
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import co.gargoyle.rocnation.R;
+import co.gargoyle.rocnation.dummy.DummyContent;
 
 
 
@@ -35,23 +18,25 @@ import co.gargoyle.rocnation.R;
 /**
  * Fragment that appears in the "content_frame", shows a planet
  */
-public class MusicFragment extends Fragment {
+public class MusicFragment extends ListFragment {
 
-	public MusicFragment() {
-		// Empty constructor required for fragment subclasses
-	}
+    public MusicFragment() {
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.fragment_music, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_music, container, false);
 
-		// int i = getArguments().getInt(ARG_PLANET_NUMBER);
-		// String planet = getResources().getStringArray(R.array.nav_array)[i];
+        getActivity().setTitle("Music");
 
-		getActivity().setTitle("Music");
+        setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(
+                getActivity(),
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                DummyContent.ITEMS));
 
-		return rootView;
-	}
+        return rootView;
+    }
 }
